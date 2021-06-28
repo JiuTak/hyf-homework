856e3ba1b67f74139c876898f3d3a81c889e2c38
jiu <aurajiu@gmail.com>
2021-06-28 16:40:46 +0200

    Implemented feedback

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -1,23 +1,23 @@
 /*---------------------------------------------------------------------------------------------*/
 // Age - ify(A future age calculator)
-yearOfBirth = 1987;
-yearFuture = 2045;
+const yearOfBirth = 1987;
+const yearFuture = 2045;
 
-age = yearFuture - yearOfBirth;
+const age = yearFuture - yearOfBirth;
 
-console.log("You will be " + age + " years old in " + yearFuture);
+console.log(`You will be ${age} years old in ${yearFuture}`);
 
 
 /*---------------------------------------------------------------------------------------------*/
 // Goodboy - Oldboy(Adog age calculator)
-dogYearOfBirth = 2021;
-dogYearFuture = 2045;
+const dogYearOfBirth = 2021;
+const dogYearFuture = 2045;
 
-dogYear = dogYearFuture - dogYearOfBirth;
-shouldShowResultInDogYears = true;
+const dogYear = dogYearFuture - dogYearOfBirth;
+const shouldShowResultInDogYears = true;
 
 if (shouldShowResultInDogYears == true) {
-    console.log("\nYour dog will be " + dogYear + " human years old in " + dogYearFuture);
+    console.log(`\nYour dog will be ${dogYear}  human years old in ${dogYearFuture}`);
 }
 
 /*---------------------------------------------------------------------------------------------*/
@@ -34,10 +34,10 @@ function gardenSizeInM2(garden) {
     return garden * 300;
 }
 
-console.log("\nJulia's current rent is: " + juliasHouseCurrentPay);
+console.log(`\nJulia's current rent is: ${juliasHouseCurrentPay}`);
 
 let juliesRightPayment = volumnInMeters(5, 11, 8) + gardenSizeInM2(70);
-console.log("Julia's right amount of rent is: " + juliesRightPayment);
+console.log("Julia's right amount of rent is: " + `${juliesRightPayment}`);
 
 if (juliesRightPayment > juliasHouseCurrentPay) {
     console.log("It is good amount to pay compare your place's size Juila!");
@@ -45,10 +45,10 @@ if (juliesRightPayment > juliasHouseCurrentPay) {
     console.log("JULIA, YOU NEED TO RE-CONSIDER YOUR PLACE");
 }
 
-console.log("\nPeter's current rent is: " + petersHouseCurrentPay);
+console.log(`\nPeter 's current rent is: ${petersHouseCurrentPay}`);
 
 var petersRightPayment = volumnInMeters(8, 10, 10) + gardenSizeInM2(100);
-console.log("Peter's right amount of rent is: " + petersRightPayment);
+console.log(`Peter 's right amount of rent is: ${petersRightPayment}`);
 
 if (petersRightPayment > petersHouseCurrentPay) {
     console.log("It is good amount to pay compare your place's size Peter!");
@@ -65,4 +65,4 @@ const randomNumber = Math.floor(Math.random() * 10);
 
 const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
 
-console.log("\nThe startup: " + startupName + " contains " + startupName.length + " characters.");
+console.log("\nThe startup: " + `${ startupName }` + " contains " + startupName.length + " characters.");
