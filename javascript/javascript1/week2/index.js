c3c266112c3255d2b5794c28d4750f5a80bdbc9b
jiu <aurajiu@gmail.com>
2021-07-06 14:42:01 +0200

    Event Application

--- javascript/javascript1/week2/index.js
+++ javascript/javascript1/week2/index.js
@@ -42,4 +42,25 @@ getFullNameWithSex("Jiu", "Tak", true, "female");
 getFullNameWithSex("Jiu", "Tak", false, "female");
 getFullNameWithSex("Jiu", "Tak", true, "male");
 getFullNameWithSex("Jiu", "Tak", false, "male");
+
+
 //===================================================================================================================
+// Event application
+
+
+const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+function getEventWeekday(days) {
+    const today = new Date();
+    // console.log(today)
+    const dayNumber = (today.getDay() + parseInt(days)) % weekDays.length;
+
+    console.log(weekDays[dayNumber])
+}
+
+// With todays weekday a tuesday
+getEventWeekday(9); // Logs out "Thursday"
+
+// With todays weekday a Friday
+getEventWeekday(1); // Logs out "Sunday"
