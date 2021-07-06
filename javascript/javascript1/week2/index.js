2cb55f9cf0d9eaf801c3a4be80b3f0ae0ed3a67a
jiu <aurajiu@gmail.com>
2021-07-06 12:05:29 +0200

    when it comes woman using formal full name

--- javascript/javascript1/week2/index.js
+++ javascript/javascript1/week2/index.js
@@ -7,21 +7,39 @@
 
 // console.log(getFullName("Benjamin", "Hughes"));
 
-
-
 // Formal fullname
 
 function getFullName(firstName, surName, useFormalName) {
     if (useFormalName === true) {
         console.log(`Lord ${firstName} ${surName}`);
-        debugger
-    } else {
+    } else if (useFormalName === false) {
+        console.log(firstName + " " + surName);
+    } else
         console.log("Need to put Firstname and Surname.")
-        debugger
-    }
+
 }
 
-getFullName("Benjamin", "Hughes", true);
+getFullName("Benjamin", "Hughes", true); // when using formal name
+getFullName("Benjamin", "Hughes", false); // without formal name
 
 
+// if is female?
+
+function getFullNameWithSex(firstName, surName, useFormalName, sex) {
+    if (sex === "female" && useFormalName === true) {
+        console.log(`Lady ${firstName} ${surName}`);
+    } else if (sex === "female" && useFormalName === false) {
+        console.log(firstName + ` ` + surName);
+    } else if (sex === "male" && useFormalName === true) {
+        console.log(`Lord ${firstName} ${surName}`);
+    } else if (sex === "male" && useFormalName === false) {
+        console.log(`${firstName} ${surName}`);
+    } else
+        console.log("Please put firstname and Surname correctly");
+}
+
+getFullNameWithSex("Jiu", "Tak", true, "female");
+getFullNameWithSex("Jiu", "Tak", false, "female");
+getFullNameWithSex("Jiu", "Tak", true, "male");
+getFullNameWithSex("Jiu", "Tak", false, "male");
 //===================================================================================================================
