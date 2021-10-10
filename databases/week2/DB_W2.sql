89565918f386c2fb2c655a96c76327516670ef5c
Jiu Tak <aurajiu@gmail.com>
2021-10-10 09:56:43 +0100

    comparing values from Mentor's comment

--- databases/week2/DB_W2.sql
+++ databases/week2/DB_W2.sql
@@ -1,8 +1,4 @@
---==============================================================
---**************************************************************
 --                  PART1: WORKING WITH TASKS
---**************************************************************
---==============================================================
 USE hyf_lesson1;
 INSERT INTO
   task (
@@ -28,54 +24,38 @@ SELECT
   *
 FROM
   task;
-
---===============================
 -- Change the title of a task
---===============================
 UPDATE
   task
 SET
   title = 'grocery shop from Nemlig'
 WHERE
-  status_id = 3 AND title LIKE "%dog%";
-
---===============================
+  status_id = 3
+  AND title LIKE "%dog%";
 --  Change a task due date
---===============================
-
 UPDATE
   task
 SET
   due_date = '2021-12-31'
 WHERE
   due_date IS NULL;
-
-
-
---===============================
 --  Change a task status
---===============================
-
 UPDATE
   task
 SET
   status_id = 2
 WHERE
-  title LIKE "%database%" AND description IS null;
-
---===============================
+  title LIKE "%database%"
+  AND description IS null;
 --  Make a task as complete
---===============================
-
-UPDATE task
-SET status_id = 3
-WHERE title LIKE "%hyf%";
-
---===============================
+UPDATE
+  task
+SET
+  status_id = 3
+WHERE
+  title LIKE "%hyf%";
 --  Delete a task
---===============================
-
-DELETE 
-FROM task 
-WHERE id = 7;
- 
+DELETE FROM
+  task
+WHERE
+  id = 7;
--- databases/week2/school_db.sql
+++ databases/week2/school_db.sql
@@ -1,31 +1,24 @@
---===============================
 -- PART2: SCHOOL DATABASE
---===============================
 --CREATE DATABASE school_db;
 USE school_db;
 CREATE TABLE `class` (
   `id` int(10) AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(30) NOT NULL,
-  `begins` DATE  NOT NULL,
-  `ends` DATE  NOT NULL
+  `begins` DATE NOT NULL,
+  `ends` DATE NOT NULL
 );
-
-
 CREATE TABLE `student` (
   `id` int(10) AUTO_INCREMENT PRIMARY KEY,
-  `name` VARCHAR(50)  NOT NULL,
-  `email` VARCHAR(25)  NOT NULL,
-  `phone` int(8)  NOT NULL,
-  `class_id` int(10)  NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(25) NOT NULL,
+  `phone` int(8) NOT NULL,
+  `class_id` int(10) NOT NULL,
   FOREIGN KEY (class_id) REFERENCES class(id) ON DELETE CASCADE
 );
-
-
 -- Create an index on the name column of the student table.
 CREATE UNIQUE INDEX name_index ON student(name);
-
 -- Add a new column to the class table named status which can only have the following values: not-started, ongoing, finished (hint: enumerations).
-
-ALTER TABLE class
-ADD status ENUM ('not-started', 'ongoing', 'finished' )
-
+ALTER TABLE
+  class
+ADD
+  status ENUM ('not-started', 'ongoing', 'finished')
