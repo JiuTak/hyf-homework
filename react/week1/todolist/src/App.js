81249a09c94f1e82165a3c410e6109c14c8fd56b
Jiu Tak <aurajiu@gmail.com>
2021-11-06 16:34:49 +0100

    Setting up React

---
+++ react/week1/todolist/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const TodoList = () => {
+  return (
+    <span> Let us see if it is works </span>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+     <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
