23d69508d4118706c7c9d54e840d0b3d05627687
Jiu Tak <aurajiu@gmail.com>
2021-11-08 13:32:03 +0100

    React week2 inclass and cleaning up

---
+++ react/week2/carousel/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ImageCarousel from "./components/ImageCarousel";
+
+function App() {
+  return (
+    <>
+      <h1>Img Karusel</h1>
+      <ImageCarousel />
+    </>
+  );
+}
+
+export default App;
